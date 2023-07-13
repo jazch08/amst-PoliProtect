7597cb3cfaefd65bd77ff15459ea731f5e10db5e
Jorge <jazapata@espol.edu.ec>
2023-07-13 01:07:17 -0500

    Inicio Sesion

---
+++ app/activity_iniciar_sesion.java
@@ -0,0 +1,2 @@
+public class activity_iniciar_sesion {
+}
--- app/src/main/java/com/example/amst_proyecto/iniciar_sesion.java
+++ app/src/main/java/com/example/amst_proyecto/iniciar_sesion.java
@@ -1,19 +1,44 @@
 package com.example.amst_proyecto;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.InputType;
 import android.text.method.PasswordTransformationMethod;
 import android.view.View;
+import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 
 
 public class iniciar_sesion extends AppCompatActivity {
-
+    private static final int RC_SIGN_IN = 9001;
+    private GoogleSignInClient mGoogleSignInClient;
+    private Button btnIniciarSesion;
+    private Button btnIniciarSesionGoogle;
+    private FirebaseAuth mAuth;
+    private GoogleApiClient mGoogleApiClient;
+    private EditText etUsuario;
     CheckBox checkboxShowPassword;
     EditText editTextPassword;
 
@@ -26,16 +51,14 @@ public class iniciar_sesion extends AppCompatActivity {
         // Declaracion de los elementos de la interfaz grafica
         checkboxShowPassword = findViewById(R.id.idCheckboxShowPassword);
         editTextPassword = findViewById(R.id.idEditTextPassword);
-
-
-
-
+        etUsuario = findViewById(R.id.editTextText);
+        btnIniciarSesion = findViewById(R.id.button);
+        btnIniciarSesionGoogle = findViewById(R.id.button2);
         checkboxShowPassword.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 // Acciones a realizar cuando cambia el estado del CheckBox
                 if (isChecked) {
-
                     editTextPassword.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD);
                 } else {
                     editTextPassword.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
@@ -43,22 +66,89 @@ public class iniciar_sesion extends AppCompatActivity {
                 }
             }
         });
+        mAuth = FirebaseAuth.getInstance();
 
+        btnIniciarSesion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                iniciarSesion();
+            }
+        });
+
+        btnIniciarSesionGoogle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                iniciarSesionConGoogle();
+            }
+        });
+
+        // Configurar opciones de inicio de sesión con Google
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+
+        // Crear el cliente de inicio de sesión con Google
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
     }
 
-    public void iniciarSesionGoogle(View v){
+    private void iniciarSesion() {
+        String usuario = etUsuario.getText().toString();
+        String contraseña = editTextPassword.getText().toString();
 
+        mAuth.signInWithEmailAndPassword(usuario, contraseña)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            redirigirAMenu();
+                        } else {
+                            Toast.makeText(iniciar_sesion.this, "Error al iniciar sesión", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
     }
 
-    public void onRegisterClick(View v){
-
+    private void iniciarSesionConGoogle() {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
     }
 
-    public void onOlvidadoClick(View v){
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
 
+        if (requestCode == RC_SIGN_IN) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            try {
+                // Inicio de sesión con Google exitoso, autenticar con Firebase
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                firebaseAuthWithGoogle(account);
+            } catch (ApiException e) {
+                Toast.makeText(this, "Error al iniciar sesión con Google", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 
-    public void onIniciarSesionClick(View v){
-
+    private void firebaseAuthWithGoogle(GoogleSignInAccount acct) {
+        AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // El inicio de sesión fue exitoso, redirige al usuario al menú
+                            redirigirAMenu();
+                        } else {
+                            // El inicio de sesión falló, muestra un mensaje de error
+                            Toast.makeText(iniciar_sesion.this, "Error al iniciar sesión con Google", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+    private void redirigirAMenu() {
+        Intent intent = new Intent(iniciar_sesion.this, entorno_principal.class);
+        startActivity(intent);
+        finish();
     }
 }
